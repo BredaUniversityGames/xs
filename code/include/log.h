5f249816d892997e470acbea33e7f62f0e17d3fc
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-04-26 16:22:56 +0200

    [Fix] Platform independent cpp's building on PS5

--- code/include/log.h
+++ code/include/log.h
@@ -1,8 +1,12 @@
 #pragma once
-#if defined(APIENTRY) && defined(_WIN32)
-#undef APIENTRY
+#if defined(PLATFORM_PC) || defined(PLATFORM_SWITCH)
+	#if defined(APIENTRY) && defined(_WIN32)
+	#undef APIENTRY
+	#endif
+	#include <spdlog/spdlog.h>
+#elif defined(PLATFORM_PS5)
+	#include <spdlog/fmt/fmt.h>
 #endif
-#include <spdlog/spdlog.h>
 
 namespace xs
 {
@@ -13,16 +17,54 @@ public:
 	static void initialize();
 	
 	template<typename FormatString, typename... Args>
-	static void info(const FormatString& fmt, const Args&... args)
-	{ spdlog::info(fmt, args...); }
+	static void info(const FormatString& fmt, const Args&... args);
 
 	template<typename FormatString, typename... Args>
-	static void warn(const FormatString& fmt, const Args&... args)
-	{ spdlog::warn(fmt, args...); }
+	static void warn(const FormatString& fmt, const Args&... args);
 
 	template<typename FormatString, typename... Args>
-	static void error(const FormatString& fmt, const Args&... args)
-	{ spdlog::error(fmt, args...); }
+	static void error(const FormatString& fmt, const Args&... args);
+
+
 };
+
+#if defined(PLATFORM_PC) || defined(PLATFORM_SWITCH)
+
+template<typename FormatString, typename ...Args>
+inline void log::info(const FormatString& fmt, const Args & ...args)
+{
+	spdlog::info(fmt, args...);
+}
+
+template<typename FormatString, typename ...Args>
+inline void log::warn(const FormatString& fmt, const Args & ...args)
+{
+	spdlog::warn(fmt, args...);
+}
+
+template<typename FormatString, typename ...Args>
+inline void log::error(const FormatString& fmt, const Args & ...args)
+{
+	spdlog::error(fmt, args...);
+}
+
+#elif defined(PLATFORM_PS5)
+
+template<typename FormatString, typename ...Args>
+inline void log::info(const FormatString& fmt, const Args & ...args)
+{
+}
+
+template<typename FormatString, typename ...Args>
+inline void log::warn(const FormatString& fmt, const Args & ...args)
+{
+}
+
+template<typename FormatString, typename ...Args>
+inline void log::error(const FormatString& fmt, const Args & ...args)
+{
+}
+
+#endif
 
 }
