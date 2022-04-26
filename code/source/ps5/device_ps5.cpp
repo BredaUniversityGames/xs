0cdfba56645aaa4a4c5eb0eab474a7ec40172096
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-04-26 16:34:51 +0200

    [Add] Empty PS5 files

---
+++ code/source/ps5/device_ps5.cpp
@@ -0,0 +1,31 @@
+#include <device.h>
+#include <log.h>
+#include <opengl.h>
+#include <configuration.h>
+
+namespace xs::device::internal
+{
+}
+
+using namespace xs;
+
+
+void device::initialize()
+{
+}
+
+void device::shutdown()
+{
+}
+
+void device::swap_buffers()
+{
+}
+
+void device::poll_events()
+{
+}
+
+bool device::should_close()
+{
+}
---
+++ code/source/ps5/input_ps5.cpp
@@ -0,0 +1,42 @@
+#include "input.h"
+
+namespace xs::input::internal
+{
+}
+
+void xs::input::initialize()
+{
+}
+
+void xs::input::shutdown()
+{
+}
+
+void xs::input::update()
+{
+}
+
+double xs::input::get_axis(int axis)
+{
+	return 0.0;
+}
+
+bool xs::input::get_button(int button)
+{
+	return false;
+}
+
+bool xs::input::get_button_once(int button)
+{
+	return false;
+}
+
+bool xs::input::get_key(int key)
+{
+	return false;
+}
+
+bool xs::input::get_key_once(int key)
+{
+	return false;
+}
