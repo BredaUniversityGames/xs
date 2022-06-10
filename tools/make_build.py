53f31a05ad5aaad60cc31cad8a3c9941f06bfc3e
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-06-10 09:19:26 +0200

    [Add] Itch build script

--- tools/make_build.py
+++ tools/make_build.py
@@ -1,5 +1,5 @@
 # This script will deploy the current Publish build 
-print("making a build...")
+print("Making a build...")
 
 import os
 from pathlib import Path
@@ -7,12 +7,13 @@ import json
 import shutil
 from shutil import copytree, ignore_patterns
 
+game_dir = "sub_optimal"
+itch_name = "SubOptimal"
+
 os.system('MSBuild xs.sln /p:Platform=PC /p:Configuration=Release')
 
 source_dir = os.getcwd()
 source_dir = source_dir + "\\executable\\x64\\Release"
-asset_dir = os.getcwd()
-asset_dir = asset_dir + "\\games"
 
 output_dir = os.getcwd()
 output_dir = output_dir + "\\build"
@@ -22,9 +23,27 @@ if os.path.exists(output_dir):
 print(source_dir)
 print(output_dir)
 
+current_dir = os.getcwd()
 if os.path.exists(source_dir):
     shutil.copytree(source_dir, output_dir, False, ignore=ignore_patterns('*.pdb', '*.ipdb', '*.iobj', '*.cap' , "*.bnvib", 'steam_appid.txt'))
-    output_dir = output_dir + "\\games"
-    shutil.copytree(asset_dir, output_dir, False, ignore=ignore_patterns('*.obj', ".bnvib"))    
+
+    asset_dir = current_dir + "\\games\\" + game_dir
+    output_dir = current_dir + "\\build\\games\\" + game_dir
+    shutil.copytree(asset_dir, output_dir, False, ignore=ignore_patterns('*.obj', ".bnvib", "*.py"))
+
+    asset_dir = current_dir + "\\games\\" + "shared"
+    output_dir = current_dir + "\\build\\games\\" + "shared"
+    shutil.copytree(asset_dir, output_dir, False, ignore=ignore_patterns('*.obj', ".bnvib", "*.py"))    
+    
+    shutil.copy(current_dir + "/init.txt", current_dir + "/build/init.txt")
+    shutil.copy(current_dir + "/tools/.itch.toml", current_dir + "/build/.itch.toml")
+
+    butler_command = "butler push build bojan/" + itch_name + ":windows-beta"
+    os.system(butler_command)
+
 else:
     print("Source dir does not exist: " + source_dir)
+
+#os.system()
+
+
