2c1984c427031f0d76228a0b87cfd42dccfcbad7
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-06-15 11:03:18 +0200

    [Update] Better register UI

--- code/include/registry.h
+++ code/include/registry.h
@@ -14,7 +14,7 @@ namespace xs::registry
 
 	void initialize();
 	void shutdown();
-	void inspect();
+	void inspect();	
 
 	double get_number(const std::string& name);
 	uint32_t get_color(const std::string& name);
