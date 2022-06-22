efee0f319add874991c8339c8162688450d786a6
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-06-22 11:48:06 +0200

    [Add] Visual profiler

--- code/source/device_swtich.cpp
+++ code/source/device_swtich.cpp
@@ -9,6 +9,7 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include "log.h"
+#include "profiler.h"
 
 using namespace xs::device;
 
@@ -198,6 +199,7 @@ void xs::device::shutdown()
 
 void xs::device::swap_buffers()
 {
+	XS_PROFILE_FUNCTION();
 	::eglSwapBuffers(display, surface);
 	glClearColor(0.0, 0.0, 0.0, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
