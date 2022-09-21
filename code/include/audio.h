1d524ec59e851486d74e33ed9ff663b5a64cc30b
Wouter van Toll <wouter@vantoll.nl>
2022-09-21 16:01:18 +0200

    Added functions for getting/setting the volume of individual channels.

--- code/include/audio.h
+++ code/include/audio.h
@@ -33,7 +33,22 @@ namespace xs::audio
 	/// <summary>
 	/// Sets the volume value for a sound group.
 	/// </summary>
-	/// <param name="group_id">The ID of the sound group for which you want to get the volume.</param>
+	/// <param name="group_id">The ID of the sound group for which you want to set the volume.</param>
 	/// <param name="value">The new volume value (in the range 0-1) for the sound group.</param>
 	void set_group_volume(int group_id, double value);
+
+	/// <summary>
+	/// Gets the current volume multiplier of a sound channel, relative to the general volume of the corresponding sound group.
+	/// </summary>
+	/// <param name="channel_id">The ID of the sound channel for which you want to get the volume.</param>
+	/// <returns>The current volume multiplier (0 or higher) for the sound channel, relative to the general volume of the corresponding sound group.</returns>
+	double get_channel_volume(int channel_id);
+
+	/// <summary>
+	/// Sets the volume multiplier for a sound channel, relative to the general volume of the corresponding sound group.
+	/// For example, setting it to 2 will make the sound on this channel play twice as loud as other sounds in the same group.
+	/// </summary>
+	/// <param name="channel_id">The ID of the sound channel for which you want to set the volume.</param>
+	/// <param name="value">The new volume multiplier (0 or higher) for the sound channel.</param>
+	void set_channel_volume(int group_id, double value);
 }
