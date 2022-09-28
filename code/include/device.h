7766a9ae8564a8b967b8dfe49c7ed6006af5d1fa
Wouter van Toll <wouter@vantoll.nl>
2022-09-28 13:49:46 +0200

    💻 Added xs::device::get_platform(), also exposed to Wren.

--- code/include/device.h
+++ code/include/device.h
@@ -9,4 +9,18 @@ namespace xs::device
 	bool should_close();
 	int get_width();
 	int get_height();
+
+	enum platform 
+	{
+		PC = 0,
+		PS5,
+		SWITCH,
+		PLATFORM_UNKNOWN
+	};
+
+	/// <summary>
+	/// Gets the target platform of the program.
+	/// Use it as an API function to let external scripts ask for the current platform at run-time.
+	/// </summary>
+	platform get_platform();
 }
