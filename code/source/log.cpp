0f7d0e0cb5934849aa0913ab132431a57f243af4
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-10-05 12:35:32 +0200

    🧡 orange start message

--- code/source/log.cpp
+++ code/source/log.cpp
@@ -25,10 +25,10 @@ void xs::log::initialize()
     spdlog::error("ERR!?");
 #endif
 
-    log::info("  __ __ _____ ");
-    log::info(" |  |  |   __|");
-    log::info(" |-   -|__   |");
-    log::info(" |__|__|_____| " + xs::version::version_string);
+    log::info("\033[33m  __ __ _____  \033[0m");
+    log::info("\033[33m |  |  |   __| \033[0m");
+    log::info("\033[33m |-   -|__   | \033[0m");
+    log::info("\033[33m |__|__|_____| \033[0m" + xs::version::version_string);
     log::info("Made with love at Breda University of Applied Sciences");
     log::info("");
 }
