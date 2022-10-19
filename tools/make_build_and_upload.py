f69489d11751bf383bd202ce694cb9961577b3d5
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-10-19 15:48:08 +0200

    🛒 itch.io upload script

---
+++ tools/make_build_and_upload.py
@@ -0,0 +1,48 @@
+# This script will deploy the current Publish build 
+print("Making a build...")
+
+import os
+import shutil
+from pathlib import Path
+from shutil import copytree, ignore_patterns
+
+game_dir = "hello"
+itch_name = "xs"
+
+os.system('MSBuild xs.sln /p:Platform=PC /p:Configuration=Test')
+
+source_dir = os.getcwd()
+source_dir = source_dir + "\\executable\\x64\\Develop"
+
+output_dir = os.getcwd()
+output_dir = output_dir + "\\build"
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+
+    
+print(source_dir)
+print(output_dir)
+
+current_dir = os.getcwd()
+if os.path.exists(source_dir):
+    
+    shutil.copytree(source_dir, output_dir, False, ignore=ignore_patterns('*.pdb', '*.ipdb', '*.iobj', '*.cap' , "*.bnvib", 'steam_appid.txt', '*.ilk'))
+
+    asset_dir = current_dir + "\\games\\" + game_dir
+    output_dir = current_dir + "\\build\\games\\" + game_dir
+    shutil.copytree(asset_dir, output_dir, False, ignore=ignore_patterns('*.obj', ".bnvib", "*.py"))
+
+    asset_dir = current_dir + "\\games\\" + "shared"
+    output_dir = current_dir + "\\build\\games\\" + "shared"
+    shutil.copytree(asset_dir, output_dir, False, ignore=ignore_patterns('*.obj', ".bnvib", "*.py"))    
+    
+    toml_src = current_dir + "\\tools\\.itch.toml"
+    toml_dst = current_dir + "\\build\\.itch.toml"
+    shutil.copyfile(toml_src, toml_dst)
+    
+    butler_command = "butler push build xs-engine/" + itch_name + ":win"
+    os.system(butler_command)
+
+else:
+    print("Source dir does not exist: " + source_dir)
+
