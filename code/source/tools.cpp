687cd95db32cbfe800951596e5c76d7483ba1b5d
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-12-28 12:50:51 +0100

    🌴 games can be put in a nested folder

--- code/source/tools.cpp
+++ code/source/tools.cpp
@@ -21,6 +21,33 @@ std::string xs::tools::string_replace(
 	return result;
 }
 
+std::vector<std::string> xs::tools::string_split(
+	const std::string& source,
+	const char* delimiter,
+	bool keep_empty)
+{
+	std::vector<std::string> results;
+
+	size_t prev = 0;
+	size_t next = 0;
+
+	while ((next = source.find_first_of(delimiter, prev)) != std::string::npos)
+	{
+		if (keep_empty || (next - prev != 0))
+		{
+			results.push_back(source.substr(prev, next - prev));
+		}
+		prev = next + 1;
+	}
+
+	if (prev < source.size())
+	{
+		results.push_back(source.substr(prev));
+	}
+
+	return results;
+}
+
 std::tuple<double, double, double, double> tools::parse_color(const std::string& hex_color)
 {
 	std::string color_str = hex_color;
