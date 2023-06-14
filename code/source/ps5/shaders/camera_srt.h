f32bcd672c685e8b1a7073e94d7620b980cbb53c
Bojan Endrovski <bojan.endrovski@gmail.com>
2023-06-14 16:55:16 +0200

    🕹️ ps5 alpha fixed + restructure

--- code/source/ps5/shaders/camera_srt.h
+++ code/source/ps5/shaders/camera_srt.h
@@ -4,13 +4,10 @@
 #define float4x4	 sce::Vectormath::Simd::Aos::Matrix4
 #endif
 
-struct Camera
+struct camera_srt
 {
 	float x;
 	float y;
-
 	float res_x;
 	float res_y;
-
-	float4x4 u_worldviewproj;
 };
