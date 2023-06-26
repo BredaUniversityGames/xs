c1c4de027c6b06826019daaecf2aefa1423a4b35
Bojan Endrovski <bojan.endrovski@gmail.com>
2023-06-26 21:57:44 +0200

    🎶 multiplatform (device) project for apple

---
+++ code/source/apple/macos/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  xs macOS
+//
+//  Created by Bojan Endrovski on 26/06/2023.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
