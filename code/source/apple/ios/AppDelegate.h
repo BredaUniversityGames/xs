8a6ccec4030239c5dacd218994a2d511214924b1
Bojan Endrovski <bojan.endrovski@gmail.com>
2023-06-26 21:57:44 +0200

    🎶 multiplatform (device) project for apple

---
+++ code/source/apple/ios/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  xs iOS
+//
+//  Created by Bojan Endrovski on 26/06/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+                        
+
+@end
+
