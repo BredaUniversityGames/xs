f823d18e5af688d86ab775bd1dec2867c91150f4
Bojan Endrovski <enci@users.noreply.github.com>
2024-01-15 12:27:45 +0000

    💇‍♀️ removing (visual) nesting in namespaces

--- code/include/log.h
+++ code/include/log.h
@@ -8,68 +8,65 @@
 #define USE_LOG_COLOR
 #endif
 
-namespace xs
+namespace xs::log
 {
-	namespace log
-	{
 #ifdef USE_LOG_COLOR
-		constexpr auto green = "\033[32m";
-		constexpr auto magenta = "\033[35m";
-		constexpr auto red = "\033[31m";
-		constexpr auto yellow = "\033[33m";
-		constexpr auto reset = "\033[0m";
+	constexpr auto green = "\033[32m";
+	constexpr auto magenta = "\033[35m";
+	constexpr auto red = "\033[31m";
+	constexpr auto yellow = "\033[33m";
+	constexpr auto reset = "\033[0m";
 #else
-		constexpr auto green = "";
-		constexpr auto magenta = "";
-		constexpr auto red = "";
-		constexpr auto yellow = "";
-		constexpr auto reset = "";
+	constexpr auto green = "";
+	constexpr auto magenta = "";
+	constexpr auto red = "";
+	constexpr auto yellow = "";
+	constexpr auto reset = "";
 #endif
 
-		void initialize();
+	void initialize();
 
-		template<typename FormatString, typename... Args>
-		void info(const FormatString& fmt, const Args&... args);
+	template<typename FormatString, typename... Args>
+	void info(const FormatString& fmt, const Args&... args);
 
-		template<typename FormatString, typename... Args>
-		void warn(const FormatString& fmt, const Args&... args);
+	template<typename FormatString, typename... Args>
+	void warn(const FormatString& fmt, const Args&... args);
 
-		template<typename FormatString, typename... Args>
-		void error(const FormatString& fmt, const Args&... args);
+	template<typename FormatString, typename... Args>
+	void error(const FormatString& fmt, const Args&... args);
 
-		template<typename FormatString, typename... Args>
-		void critical(const FormatString& fmt, const Args&... args);
-	}
+	template<typename FormatString, typename... Args>
+	void critical(const FormatString& fmt, const Args&... args);
+}
 
-	template<typename FormatString, typename ...Args>
-	inline void log::info(const FormatString& format, const Args & ...args)
-	{
-		printf("[%sinfo%s] ", green, reset);
-		fmt::print(fmt::runtime(format), args...);
-		printf("\n");
-	}
+template<typename FormatString, typename ...Args>
+inline void log::info(const FormatString& format, const Args & ...args)
+{
+	printf("[%sinfo%s] ", green, reset);
+	fmt::print(fmt::runtime(format), args...);
+	printf("\n");
+}
 
-	template<typename FormatString, typename ...Args>
-	inline void log::warn(const FormatString& format, const Args & ...args)
-	{
-		printf("[%swarn%s] ", magenta, reset);
-		fmt::print(fmt::runtime(format), args...);
-		printf("\n");
-	}
+template<typename FormatString, typename ...Args>
+inline void log::warn(const FormatString& format, const Args & ...args)
+{
+	printf("[%swarn%s] ", magenta, reset);
+	fmt::print(fmt::runtime(format), args...);
+	printf("\n");
+}
 
-	template<typename FormatString, typename ...Args>
-	inline void log::error(const FormatString& format, const Args & ...args)
-	{
-		printf("[%serror%s] ", red, reset);
-		fmt::print(fmt::runtime(format), args...);
-		printf("\n");
-	}
+template<typename FormatString, typename ...Args>
+inline void log::error(const FormatString& format, const Args & ...args)
+{
+	printf("[%serror%s] ", red, reset);
+	fmt::print(fmt::runtime(format), args...);
+	printf("\n");
+}
 
-	template<typename FormatString, typename ...Args>
-	inline void log::critical(const FormatString& format, const Args & ...args)
-	{
-		printf("[%serror%s] ", red, reset);
-		fmt::print(fmt::runtime(format), args...);
-		printf("\n");
-	}
+template<typename FormatString, typename ...Args>
+inline void log::critical(const FormatString& format, const Args & ...args)
+{
+	printf("[%serror%s] ", red, reset);
+	fmt::print(fmt::runtime(format), args...);
+	printf("\n");
 }
