7f844a3e122c75870680a850c6d77e2b56df9efb
Dyronix <breuck.d@buas.nl>
2024-01-15 15:08:48 +0100

    The cooker which will load in all assets for a set of folders ( starting from the "games" folder ) merge them within one chunck of data and starts the compression algorithm. After compression it will store the archive on disk

---
+++ code/include/cooker.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace xs
+{
+	namespace cooker
+	{
+		/*
+		* Cook Content
+		*
+		* This function initiates the cooking process for content by generating and compressing an archive.
+		* The generated archive is then written to disk. This is a key step in preparing content for runtime use.
+		*
+		* @param root - The root directory containing the content to be cooked.
+		* @param subDirs - A vector of subdirectories within the root to include in the cooking process.
+		* 
+		* @return True if the cooking process is successful, false otherwise.
+		*/
+		bool cook_content(const std::string& root, const std::vector<std::string>& subDirs);
+	}
+}
