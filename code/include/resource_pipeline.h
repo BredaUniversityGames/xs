09630680e1720b372680f2817752d220c6f13451
Dyronix <breuck.d@buas.nl>
2024-01-16 10:23:54 +0100

    Distinguish between binary and text files for easier integration with the fileio

--- code/include/resource_pipeline.h
+++ code/include/resource_pipeline.h
@@ -19,10 +19,11 @@ namespace xs
         struct ContentHeader
         {
             ContentHeader();
-            ContentHeader(const std::string& path, u64 fileSize);
+            ContentHeader(const std::string& path, u64 fileSize, char8 isText);
 
             char8 file_path[s_max_path];
             u64 file_size;
+            char8 is_text;
         };
 
         // ------------------------------------------------------------------------
@@ -56,10 +57,17 @@ namespace xs
         // Function to generate an archive path based on the root and subdirectories
         std::string make_archive_path(const std::string& root, const std::vector<std::string>& subDirs = {});
 
-        // Function to get the set of supported file formats
-        const std::unordered_set<std::string>& supported_file_formats();
+        // Function to get the set of supported text file formats
+        const std::unordered_set<std::string>& supported_text_file_formats();
+        // Function to get the set of supported binary file formats
+        const std::unordered_set<std::string>& supported_binary_file_formats();
 
         // Function to check if a file format is supported
         bool is_supported_file_format(const std::string& extension);
+
+        // Function that checks if a file is a binary file
+        bool is_binary_file(const std::string& extension);
+        // Function that checks if a file is a text file
+        bool is_text_file(const std::string& extension);
 	}
 }
