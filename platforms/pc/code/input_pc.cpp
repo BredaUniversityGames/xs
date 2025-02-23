203768ce0d56cd4779688adca936dab8b47a0513
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-02-23 22:53:09 +0100

    🎹 keyboard support

--- platforms/pc/code/input_pc.cpp
+++ platforms/pc/code/input_pc.cpp
@@ -49,10 +49,15 @@ struct gamepad_entry
 struct Data
 {
 	unordered_map<size_t, gamepad_entry> gamepads;
+
+	bool keys_down[SDL_SCANCODE_COUNT];
+	bool prev_keys_down[SDL_SCANCODE_COUNT];
 };
 
 gamepad_entry* get_default_gamepad();
 void update_all_gamepads();
+void update_keyboard();
+int get_usb_scancode(int key);
 Data* data = nullptr;
 
 }
@@ -74,6 +79,7 @@ void xs::input::shutdown()
 void xs::input::update(double dt)
 {
 	update_all_gamepads();
+	update_keyboard();
 }
 
 double xs::input::get_mouse_x() { return 0.0; }
@@ -106,9 +112,19 @@ void xs::input::reset_lightbar() {}
 
 bool xs::input::get_key(int key)
 {
-	return 
+	auto scanout = get_usb_scancode(key);
+	if(0 <= scanout && scanout < SDL_SCANCODE_COUNT)
+		return data->keys_down[scanout];
+	return false;
 }
-bool xs::input::get_key_once(int key) { return false; }
+
+bool xs::input::get_key_once(int key)
+{
+	auto scanout = get_usb_scancode(key);
+	if (0 <= scanout && scanout < SDL_SCANCODE_COUNT)
+		return data->keys_down[scanout] && !data->prev_keys_down[scanout];
+	return false;
+}
 
 bool xs::input::get_mouse()
 {
@@ -154,7 +170,7 @@ double xs::input::get_axis(gamepad_axis axis)
 	return value;
 }
 
-xs::input::gamepad_entry * xs::input::get_default_gamepad()
+xs::input::gamepad_entry* xs::input::get_default_gamepad()
 {
 	if (data->gamepads.size() > 0)
 		return &data->gamepads[0];
@@ -194,4 +210,146 @@ void xs::input::update_all_gamepads()
 				data->gamepads[player].gamepad.axes[i] = SDL_GetGamepadAxis(sdl_pad, (SDL_GamepadAxis)i) / 32767.0f;			
 		}		
 	}
+}
+
+void xs::input::update_keyboard()
+{
+	int count = 0;
+	const bool* keys = SDL_GetKeyboardState(&count);
+	count = min(count, (int)SDL_SCANCODE_COUNT);
+
+	auto size = count * sizeof(bool);
+
+	memcpy(data->prev_keys_down, data->keys_down, size);
+	memcpy(data->keys_down, keys, size);
+}
+
+int xs::input::get_usb_scancode(int key)
+{
+	int scanout = -1;
+	switch (key)
+	{
+	case KEY_SPACE: scanout = SDL_SCANCODE_SPACE; break;
+	case KEY_APOSTROPHE: scanout = SDL_SCANCODE_APOSTROPHE; break;
+	case KEY_COMMA: scanout = SDL_SCANCODE_COMMA; break;
+	case KEY_MINUS: scanout = SDL_SCANCODE_MINUS; break;
+	case KEY_PERIOD: scanout = SDL_SCANCODE_PERIOD; break;
+	case KEY_SLASH: scanout = SDL_SCANCODE_SLASH; break;
+	case KEY_0: scanout = SDL_SCANCODE_0; break;
+	case KEY_1: scanout = SDL_SCANCODE_1; break;
+	case KEY_2: scanout = SDL_SCANCODE_2; break;
+	case KEY_3: scanout = SDL_SCANCODE_3; break;
+	case KEY_4: scanout = SDL_SCANCODE_4; break;
+	case KEY_5: scanout = SDL_SCANCODE_5; break;
+	case KEY_6: scanout = SDL_SCANCODE_6; break;
+	case KEY_7: scanout = SDL_SCANCODE_7; break;
+	case KEY_8: scanout = SDL_SCANCODE_8; break;
+	case KEY_9: scanout = SDL_SCANCODE_9; break;
+	case KEY_SEMICOLON: scanout = SDL_SCANCODE_SEMICOLON; break;
+	//case KEY_EQUALS: scanout = SDL_SCANCODE_EQUALS; break;
+	case KEY_A: scanout = SDL_SCANCODE_A; break;
+	case KEY_B: scanout = SDL_SCANCODE_B; break;
+	case KEY_C: scanout = SDL_SCANCODE_C; break;
+	case KEY_D: scanout = SDL_SCANCODE_D; break;
+	case KEY_E: scanout = SDL_SCANCODE_E; break;
+	case KEY_F: scanout = SDL_SCANCODE_F; break;
+	case KEY_G: scanout = SDL_SCANCODE_G; break;
+	case KEY_H: scanout = SDL_SCANCODE_H; break;
+	case KEY_I: scanout = SDL_SCANCODE_I; break;
+	case KEY_J: scanout = SDL_SCANCODE_J; break;
+	case KEY_K: scanout = SDL_SCANCODE_K; break;
+	case KEY_L: scanout = SDL_SCANCODE_L; break;
+	case KEY_M: scanout = SDL_SCANCODE_M; break;
+	case KEY_N: scanout = SDL_SCANCODE_N; break;
+	case KEY_O: scanout = SDL_SCANCODE_O; break;
+	case KEY_P: scanout = SDL_SCANCODE_P; break;
+	case KEY_Q: scanout = SDL_SCANCODE_Q; break;
+	case KEY_R: scanout = SDL_SCANCODE_R; break;
+	case KEY_S: scanout = SDL_SCANCODE_S; break;
+	case KEY_T: scanout = SDL_SCANCODE_T; break;
+	case KEY_U: scanout = SDL_SCANCODE_U; break;
+	case KEY_V: scanout = SDL_SCANCODE_V; break;
+	case KEY_W: scanout = SDL_SCANCODE_W; break;
+	case KEY_X: scanout = SDL_SCANCODE_X; break;
+	case KEY_Y: scanout = SDL_SCANCODE_Y; break;
+	case KEY_Z: scanout = SDL_SCANCODE_Z; break;
+	//case KEY_LEFTBRACKET: scanout = SDL_SCANCODE_LEFTBRACKET; break;
+	case KEY_BACKSLASH: scanout = SDL_SCANCODE_BACKSLASH; break;
+	//case KEY_RIGHTBRACKET: scanout = SDL_SCANCODE_RIGHTBRACKET; break;
+	//case KEY_GRAVE: scanout = SDL_SCANCODE_GRAVE; break;
+	case KEY_ESCAPE: scanout = SDL_SCANCODE_ESCAPE; break;
+	//case KEY_RETURN: scanout = SDL_SCANCODE_RETURN; break;
+	case KEY_TAB: scanout = SDL_SCANCODE_TAB; break;
+	case KEY_BACKSPACE: scanout = SDL_SCANCODE_BACKSPACE; break;
+	case KEY_INSERT: scanout = SDL_SCANCODE_INSERT; break;
+	case KEY_DELETE: scanout = SDL_SCANCODE_DELETE; break;
+	case KEY_RIGHT: scanout = SDL_SCANCODE_RIGHT; break;
+	case KEY_LEFT: scanout = SDL_SCANCODE_LEFT; break;
+	case KEY_DOWN: scanout = SDL_SCANCODE_DOWN; break;
+	case KEY_UP: scanout = SDL_SCANCODE_UP; break;
+	//case KEY_PAGEUP: scanout = SDL_SCANCODE_PAGEUP; break;
+	//case KEY_PAGEDOWN: scanout = SDL_SCANCODE_PAGEDOWN; break;
+	case KEY_HOME: scanout = SDL_SCANCODE_HOME; break;
+	case KEY_END: scanout = SDL_SCANCODE_END; break;
+	//case KEY_CAPSLOCK: scanout = SDL_SCANCODE_CAPSLOCK; break;
+	//case KEY_SCROLLLOCK: scanout = SDL_SCANCODE_SCROLLLOCK; break;
+	//case KEY_NUMLOCKCLEAR: scanout = SDL_SCANCODE_NUMLOCKCLEAR; break;
+	//case KEY_PRINTSCREEN: scanout = SDL_SCANCODE_PRINTSCREEN; break;
+	case KEY_PAUSE: scanout = SDL_SCANCODE_PAUSE; break;
+	case KEY_F1: scanout = SDL_SCANCODE_F1; break;
+	case KEY_F2: scanout = SDL_SCANCODE_F2; break;
+	case KEY_F3: scanout = SDL_SCANCODE_F3; break;
+	case KEY_F4: scanout = SDL_SCANCODE_F4; break;
+	case KEY_F5: scanout = SDL_SCANCODE_F5; break;
+	case KEY_F6: scanout = SDL_SCANCODE_F6; break;
+	case KEY_F7: scanout = SDL_SCANCODE_F7; break;
+	case KEY_F8: scanout = SDL_SCANCODE_F8; break;
+	case KEY_F9: scanout = SDL_SCANCODE_F9; break;
+	case KEY_F10: scanout = SDL_SCANCODE_F10; break;
+	case KEY_F11: scanout = SDL_SCANCODE_F11; break;
+	case KEY_F12: scanout = SDL_SCANCODE_F12; break;
+		/*
+	case KEY_F13: scanout = SDL_SCANCODE_F13; break;
+	case KEY_F14: scanout = SDL_SCANCODE_F14; break;
+	case KEY_F15: scanout = SDL_SCANCODE_F15; break;
+	case KEY_F16: scanout = SDL_SCANCODE_F16; break;
+	case KEY_F17: scanout = SDL_SCANCODE_F17; break;
+	case KEY_F18: scanout = SDL_SCANCODE_F18; break;
+	case KEY_F19: scanout = SDL_SCANCODE_F19; break;
+	case KEY_F20: scanout = SDL_SCANCODE_F20; break;
+	case KEY_F21: scanout = SDL_SCANCODE_F21; break;
+	case KEY_F22: scanout = SDL_SCANCODE_F22; break;
+	case KEY_F23: scanout = SDL_SCANCODE_F23; break;
+	case KEY_F24: scanout = SDL_SCANCODE_F24; break;
+	case KEY_KP_0: scanout = SDL_SCANCODE_KP_0; break;
+	case KEY_KP_1: scanout = SDL_SCANCODE_KP_1; break;
+	case KEY_KP_2: scanout = SDL_SCANCODE_KP_2; break;
+	case KEY_KP_3: scanout = SDL_SCANCODE_KP_3; break;
+	case KEY_KP_4: scanout = SDL_SCANCODE_KP_4; break;
+	case KEY_KP_5: scanout = SDL_SCANCODE_KP_5; break;
+	case KEY_KP_6: scanout = SDL_SCANCODE_KP_6; break;
+	case KEY_KP_7: scanout = SDL_SCANCODE_KP_7; break;
+	case KEY_KP_8: scanout = SDL_SCANCODE_KP_8; break;
+	case KEY_KP_9: scanout = SDL_SCANCODE_KP_9; break;
+	case KEY_KP_PERIOD: scanout = SDL_SCANCODE_KP_PERIOD; break;
+	case KEY_KP_DIVIDE: scanout = SDL_SCANCODE_KP_DIVIDE; break;
+	case KEY_KP_MULTIPLY: scanout = SDL_SCANCODE_KP_MULTIPLY; break;
+	case KEY_KP_MINUS: scanout = SDL_SCANCODE_KP_MINUS; break;
+	case KEY_KP_PLUS: scanout = SDL_SCANCODE_KP_PLUS; break;
+	case KEY_KP_ENTER: scanout = SDL_SCANCODE_KP_ENTER; break;
+	case KEY_KP_EQUALS: scanout = SDL_SCANCODE_KP_EQUALS; break;
+	case KEY_LCTRL: scanout = SDL_SCANCODE_LCTRL; break;
+	case KEY_LSHIFT: scanout = SDL_SCANCODE_LSHIFT; break;
+	case KEY_LALT: scanout = SDL_SCANCODE_LALT; break;
+	case KEY_LGUI: scanout = SDL_SCANCODE_LGUI; break;
+	case KEY_RCTRL: scanout = SDL_SCANCODE_RCTRL; break;
+	case KEY_RSHIFT: scanout = SDL_SCANCODE_RSHIFT; break;
+	case KEY_RALT: scanout = SDL_SCANCODE_RALT; break;
+	case KEY_RGUI: scanout = SDL_SCANCODE_RGUI; break;
+	*/
+	default: scanout = -1; break;
+
+	}
+
+	return scanout;
 }
