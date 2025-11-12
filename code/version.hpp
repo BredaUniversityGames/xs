786e1153140e0d9aead69e4e6a4572fd4e00b540
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-11-12 16:08:54 +0100

    🪟 a bit (even) simpler ui

--- code/version.hpp
+++ code/version.hpp
@@ -5,10 +5,10 @@ namespace xs::version
 {
     // Version components (single source of truth)
     constexpr int XS_VERSION_YEAR = 25;
-    constexpr int XS_VERSION_BUILD = 161;
+    constexpr int XS_VERSION_BUILD = 162;
 
     // Short commit hash
-    constexpr const char* XS_COMMIT_HASH = "03ed586";
+    constexpr const char* XS_COMMIT_HASH = "c6af9e0";
 
     // Version string builder function (implemented in version.cpp)
     // Builds version strings from the integer components above
