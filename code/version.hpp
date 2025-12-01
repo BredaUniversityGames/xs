dcdde53fb6148e87bfb874c7d3867927c958f0fb
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-12-01 10:16:24 +0100

    🤩 small ui color tweak

--- code/version.hpp
+++ code/version.hpp
@@ -5,10 +5,10 @@ namespace xs::version
 {
     // Version components (single source of truth)
     constexpr int XS_VERSION_YEAR = 25;
-    constexpr int XS_VERSION_BUILD = 197;
+    constexpr int XS_VERSION_BUILD = 198;
 
     // Short commit hash
-    constexpr const char* XS_COMMIT_HASH = "5e0f648";
+    constexpr const char* XS_COMMIT_HASH = "d5ce719";
 
     // Version string builder function (implemented in version.cpp)
     // Builds version strings from the integer components above
