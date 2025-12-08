b353597be794112058caae0f9e43a90acead26a7
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-12-08 11:19:29 +0100

    🍷 corrent (no) alpha when compositing

--- code/opengl/render_gl.cpp
+++ code/opengl/render_gl.cpp
@@ -523,7 +523,7 @@ void xs::render::create_frame_buffers()
 	glBindFramebuffer(GL_FRAMEBUFFER, render_fbo);	
 	glGenTextures(1, &render_texture);
 	glBindTexture(GL_TEXTURE_2D, render_texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, render_texture, 0);
