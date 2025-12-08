77a55222ca78c9571faf381b8dfac650e567a672
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-12-08 11:18:37 +0100

    🐧 using dispatch and commands on linux too

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,4 @@
 #include "xs.hpp"
-#include "device.hpp"
-#include <chrono>
-#include <algorithm>
-#include "main.hpp"
-
-using namespace std;
-
 
 #if defined(PLATFORM_PC) || defined(PLATFORM_LINUX)
 
@@ -20,24 +13,6 @@ extern "C"
 
 int main(int argc, char* argv[])
 {
-	return xs::main(argc, argv);
+	return xs::dispatch(argc, argv);
 }
 #endif
-
-
-int xs::main(int argc, char* argv[])
-{
-	xs::initialize();
-    auto prev_time = chrono::high_resolution_clock::now();
-    while (!device::should_close())
-    {
-        auto current_time = chrono::high_resolution_clock::now();
-        auto elapsed = current_time - prev_time;
-        prev_time = current_time;
-        auto dt = std::chrono::duration<double>(elapsed).count();
-        dt = std::min(dt, 0.03333);
-        xs::update((float)dt);
-    }
-	xs::shutdown();
-	return 0;
-}
--- code/main.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace xs
-{
-    int main(int argc, char* argv[]);
-}
