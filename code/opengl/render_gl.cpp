4d779b1ca642c4e1a25e72030869a9d11edb62c0
Bojan Endrovski <bojan.endrovski@gmail.com>
2026-03-10 17:48:24 +0100

    📺 fixed the crt post process

--- code/opengl/render_gl.cpp
+++ code/opengl/render_gl.cpp
@@ -579,6 +579,8 @@ void xs::render::create_frame_buffers()
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, render_texture, 0);
 
 	// Create the depth buffer
