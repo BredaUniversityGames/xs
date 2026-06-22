efc827fb94a832c23f4dfd9a52766357ad5bf2c0
Bojan Endrovski <bojan.endrovski@gmail.com>
2026-06-22 14:03:35 +0200

    🚢 version bump

--- code/version.hpp
+++ code/version.hpp
@@ -5,10 +5,10 @@ namespace xs::version
 {
     // Version components (single source of truth)
     constexpr int XS_VERSION_YEAR = 26;
-    constexpr int XS_VERSION_BUILD = 118;
+    constexpr int XS_VERSION_BUILD = 119;
 
     // Short commit hash
-    constexpr const char* XS_COMMIT_HASH = "1020105";
+    constexpr const char* XS_COMMIT_HASH = "159074d";
 
     // Version string builder function (implemented in version.cpp)
     // Builds version strings from the integer components above
