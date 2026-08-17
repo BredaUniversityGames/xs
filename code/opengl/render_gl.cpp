0725194a53e8f4e50bb23c2698cbf35cffe02509
Bojan Endrovski <bojan.endrovski@gmail.com>
2026-08-17 17:01:20 +0200

    🐘 new sdl3

--- code/opengl/render_gl.cpp
+++ code/opengl/render_gl.cpp
@@ -651,8 +651,8 @@ void xs::render::create_frame_buffers()
 		glGenTextures(1, &postprocess_texture);
 		glBindTexture(GL_TEXTURE_2D, postprocess_texture);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, postprocess_w, postprocess_h, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, postprocess_texture, 0);
 
 		unsigned int postprocess_attachments[1] = { GL_COLOR_ATTACHMENT0 };
